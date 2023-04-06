c075a13e3fc05fdf73a43bc654e0aecfbd1e7cca
archieaustin <55399025+archieaustin@users.noreply.github.com>
2023-04-06 03:21:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
-
-  // // Get a reference to the SVG element
-  // var svgElement = document.getElementById('svg14533');
-
-  // // Initialize svgpanzoom with default options
-  // var panZoomInstance = svgPanZoom(svgElement);
-
-  // // Optionally, customize the zoom behavior
-  // panZoomInstance.setMinZoom(0.5);
-  // panZoomInstance.setMaxZoom(5);
-
-  // // Optionally, add event listeners for zooming and panning
-  // svgElement.addEventListener('pan', function(e) {
-  //   console.log('panning', e.detail);
-  // });
-  // svgElement.addEventListener('zoom', function(e) {
-  //   console.log('zooming', e.detail);
-  // });
-
-const svg = document.querySelector('#svg14533');
-const hammertime = new Hammer(svg);
-
-let lastPosX = 0;
-let lastPosY = 0;
-
-hammertime.on('pan', function(event) {
-  const dx = event.deltaX - lastPosX;
-  const dy = event.deltaY - lastPosY;
-
-  svg.style.transform = `translate(${dx}px, ${dy}px)`;
-
-  lastPosX = event.deltaX;
-  lastPosY = event.deltaY;
-});
-
-let lastScale = 1;
-
-hammertime.on('pinch', function(event) {
-  const scale = Math.max(1, Math.min(lastScale * event.scale, 10));
-
-  svg.style.transform = `scale(${scale})`;
-
-  lastScale = scale;
-});
-
-
-
+function changeColour(className) {
+  var branches = document.querySelectorAll('.' + className);
+   
+  for (var i = 0; i < branches.length; i++) {
+    var branch = branches[i];
+    if (branch.getAttribute('pressed') === 'false') {
+      branch.setAttribute('data-original-fill', branch.getAttribute('fill'));
+      branch.setAttribute('fill', 'grey');
+      branch.setAttribute('pressed', 'true');
+    } else {
+      branch.setAttribute('fill', branch.getAttribute('data-original-fill'));
+      branch.setAttribute('pressed', 'false');
+    }
+  }
+}
